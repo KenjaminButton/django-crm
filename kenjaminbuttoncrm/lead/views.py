929b10487f65afaf16ce1fcc40739a948f733372
kendrickPC <kennethpchang@gmail.com>
2023-07-05 02:06:32 +0800

    Created views and forms for leads

--- kenjaminbuttoncrm/lead/views.py
+++ kenjaminbuttoncrm/lead/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import AddLeadForm
+
+
+@login_required
+def add_lead(request):
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST)
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+            return redirect('dashboard')
+    else:
+        form = AddLeadForm()
+
+    return render(request, 'lead/addlead.html', {
+        'form': form
+    })
